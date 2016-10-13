e79466c72121f8952c79bd9e525357efad059fab
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-10-13 12:23:57 +0100

    Use the scraped page archive in the scraper

--- scraper.rb
+++ scraper.rb
@@ -8,8 +8,9 @@ require 'open-uri'
 
 # require 'colorize'
 require 'pry'
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+require 'scraped_page_archive/open-uri'
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
